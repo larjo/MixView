04cd4f64b51c56434dce46994f63573abf6b20dd
Lars Jonsson Baker <lars.o.baker@gmail.com>
2013-02-21 11:25:48 +0100

    fixed low level parsers

--- main.hs
+++ main.hs
@@ -1,19 +1,20 @@
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy as BL
---import qualified Data.ByteString as B
+import qualified Data.ByteString as B
 import Data.Binary.Get
 import Data.Word
 import Control.Applicative
 
+-- The datastructure that represents a plylist in Riff-format
 type Format = String
 type Id = String
-type RawData = String
---type RawData = BL.ByteString
+type RawData = String --type RawData = BL.ByteString
 
 data Chunk = Data Id RawData | List Chunks
 data Chunks = Chunks Format [Chunk]
 data Riff = Riff Chunks
 
+-- functions for pretty printing the playlist
 instance Show Chunk where
    show (Data i d) = "(" ++ i ++ ":" ++ d ++ ")"   
    show (List cs) = show cs
@@ -24,39 +25,30 @@ instance Show Chunks where
 instance Show Riff where
    show (Riff cs) = show cs
 
-parseRawHeader :: Get (String, Int)
-parseRawHeader = do
-  ident <- B8.unpack <$> getByteString 4
-  len <- fromIntegral <$> getWord32le
-  return (ident, len)
+-- low level parsing functions
+parseFourCC :: Get String
+parseFourCC = B8.unpack <$> getByteString 4
 
-parseRawFormat :: Get (String)
-parseRawHeader = do
-  ident <- B8.unpack <$> getByteString 4
-  return ident
+parseLength :: Get Int
+parseLength = fromIntegral <$> getWord32le
 
-parseRawFormat :: Get (String)
-parseRawHeader = do
-  ident <- B8.unpack <$> getByteString 4
-  return ident
+parseData :: Int -> Get B.ByteString
+parseData len = do
+  dat <- getByteString len
+  getByteString $ len `mod` 2 -- skip padding
+  return dat
 
-  
-parseRawChunk :: Get (String, String, BL.ByteString)
-parseRawHeader = do
-  ident <- B8.unpack <$> getByteString 4
-  len <- fromIntegral <$> getWord32le
-  format <- B8.unpack <$> getByteString 4
-  dat <- getLazyByteString (len - 4)
-  getLazyByteString $ len `mod` 2
-  return (ident, format, dat)
-
-  
+parse :: Get (String, Int, String)
+parse = do
+  h <- parseFourCC
+  l <- parseLength
+  h2 <- parseFourCC
+  return (h, l, h2)
 main :: IO ()
 main = do
   input <- BL.getContents
-  let (s1, s2, d1) = runGet parseRawHeader input
-  let (s3, _d2) = runGet parseRawData d1
-  print (s1, s2, s3)
+  let r = runGet parse input
+  print r
   
 test = show r 
   where
