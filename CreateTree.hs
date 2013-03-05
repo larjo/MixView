09645be3117c0cd58f35219acaaa4c8001c0e89e
Lars Jonsson Baker <lars.o.baker@gmail.com>
2013-03-05 13:40:28 +0100

    added createtree

---
+++ CreateTree.hs
@@ -0,0 +1,23 @@
+import qualified Data.ByteString.Lazy as BL
+import Data.Binary.Get
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
+
+import RiffTokens
+
+type Id = String
+type Len = Int
+type Format = String
+type RawData = ByteString
+                                     
+data Chunk = Chunks Id Format [Chunk]
+           | RiffData Id RawData
+
+createTree :: [Token] -> Chunk
+createTree _ = Chunks "" "" []
+
+showTree :: Chunk -> [String]
+showTree _ = []
+
+main :: IO ()
+main = BL.getContents >>= mapM_ putStrLn . showTree . createTree . runGet parseTokens
