9e1c9d7af342658ca539a7c559c50ebb8011a773
Lars Jonsson Baker <lars.o.baker@gmail.com>
2013-03-13 14:38:09 +0100

    refactored listtokens.hs

--- ListTokens.hs
+++ ListTokens.hs
@@ -1,21 +1,11 @@
 import Data.Binary.Get (runGet)
 import qualified Data.ByteString.Lazy as BL (ByteString, readFile, getContents)
 import RiffTokens
-                       
-showListInfo :: String -> List -> String
-showListInfo i l = i ++ ":" ++ listFormat l ++ "(" ++ show (listLength l) ++ ")"
-
-showChunk :: Chunk -> String
-showChunk (DataChunk d) = dataId d ++ "(" ++ show (dataLength d) ++ ")"
-showChunk (ListChunk x) = showListInfo "LIST" x
-
-printChunk :: Chunk -> IO ()
-printChunk = putStrLn . showChunk
 
 printRiff :: RiffChunks -> IO ()
 printRiff (RiffChunks l cs) = do
-    putStrLn $ showListInfo "RIFF" l
-    mapM_ printChunk cs
+    print l
+    mapM_ print cs
 
 getChunks :: BL.ByteString -> IO RiffChunks
 getChunks = return . runGet parseRiffChunks
