b0247f920693b98013a46ede21c2523ef6b9ec14
Lars Baker <lars.o.baker@gmail.com>
2014-01-07 23:58:24 +0100

    added id3 to build script

--- buildall.bat
+++ buildall.bat
@@ -1,7 +1,6 @@
 ghc -fforce-recomp -o bin\ListFiles.exe --make ListFiles -outputdir bin -Wall -Werror
 ghc -fforce-recomp -o bin\ListTokens.exe --make ListTokens -outputdir bin -Wall -Werror
 ghc -fforce-recomp -o bin\CreateTree.exe --make  CreateTree -outputdir bin -Wall -Werror
-ghc -fforce-recomp -o bin\Main.exe --make  Main -outputdir bin -Wall -Werror
-
+ghc -fforce-recomp -o bin\ID3.exe --make  ID3 -outputdir bin -Wall -Werror
 
 hlint .
