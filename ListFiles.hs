30290f2fec934fe0edc1aa065493cfa36bd5407c
= <=>
2014-07-05 00:51:01 +0200

    simpified ListFiles.hs

--- ListFiles.hs
+++ ListFiles.hs
@@ -2,23 +2,19 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf16LE)
 import Data.Binary.Get
-
+import Data.Maybe
 import RiffTokens
 import Id3
 
 getChunks :: RiffChunks -> [Chunk]
 getChunks (RiffChunks _ cs) = cs
 
-isFilename :: Chunk -> Bool
-isFilename (DataChunk (Data "TRKF" d)) = True
-isFilename _ = False
-
-chunkToFilename :: Chunk -> String
+chunkToFilename :: Chunk -> Maybe String
 chunkToFilename (DataChunk (Data "TRKF" d)) =
-    showRaw d
+    Just $ showRaw d
   where
     showRaw = T.unpack . T.init . decodeUtf16LE
-chunkToFilename _ = ""
+chunkToFilename _ = Nothing
 
 main :: IO ()
-main = BL.getContents >>= mapM_ putStrLn . map chunkToFilename . filter isFilename . getChunks . runGet parseRiffChunks
+main = BL.getContents >>= mapM_ putStrLn . catMaybes . map chunkToFilename . getChunks . runGet parseRiffChunks
