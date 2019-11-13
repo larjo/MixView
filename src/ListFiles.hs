cfb56b797fd6e58dc684437334f2080a7605c320
Lars Baker <lars.baker@uc.se>
2019-11-13 13:13:59 +0100

    Add mxm

--- src/ListFiles.hs
+++ src/ListFiles.hs
@@ -13,6 +13,8 @@ getChunks (RiffChunks _ cs) = cs
 
 chunkToFilename :: Chunk -> Maybe String
 chunkToFilename (DataChunk (Data "TRKF" d)) = Just . T.unpack . T.init . decodeUtf16LE $ d
+chunkToFilename (DataChunk (Data "info" d)) = Just . show $ d
+chunkToFilename (DataChunk (Data "prof" d)) = Just . show $ d
 chunkToFilename _ = Nothing
 
 listFiles :: BL.ByteString -> String
