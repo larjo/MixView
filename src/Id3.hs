b042791856f828258a3922819b7af2e407a51cbe
Lars Baker <lars.baker@uc.se>
2019-11-13 10:42:29 +0100

    Parse lengths correctly

--- src/Id3.hs
+++ src/Id3.hs
@@ -7,7 +7,7 @@ import           Control.Monad.Loops   (whileM)
 import           Data.Binary.Get       (Get, bytesRead, getByteString,
                                         getWord32be, getWord8, lookAhead,
                                         runGet, skip)
-import qualified Data.ByteString       as B (any, ByteString, drop , reverse, uncons )
+import qualified Data.ByteString       as B (ByteString, all, drop , reverse, uncons )
 import           Data.ByteString.Char8 (unpack)
 import qualified Data.ByteString.Lazy  as BL (ByteString, getContents)
 import qualified Data.ByteString.Encoding as BE (decode, latin1, utf16, utf8)
@@ -16,7 +16,8 @@ import qualified Data.Map              as M (Map, empty, findWithDefault,
                                              insert)
 import qualified Data.Text             as T (init, null, unpack, last)
 import           Data.Text.Encoding    (decodeUtf16LEWith, decodeUtf16BEWith)
-import           Data.Maybe            (fromMaybe)
+import           Data.Maybe            (fromMaybe, catMaybes)
+
 data Frame = Frame String String
 type FrameMap = M.Map String String
 
@@ -25,41 +26,47 @@ getHeader = unpack <$> getByteString 3
 
 getVersion :: Get String
 getVersion = do
-    words <- replicateM 2 getWord8
-    return (intercalate "." ("2" : map show words))
+    v1 <- show <$> getWord8
+    v2 <- show <$> getWord8
+    return $ "2." ++ v1 ++ "." ++ v2
 
 removeTerminator :: Int -> B.ByteString -> B.ByteString
 removeTerminator n = B.reverse . B.drop n . B.reverse 
---removeTerminator = B.reverse . B.dropWhile (\w8 -> w8 == 0) . B.reverse
 
-showRaw :: B.ByteString -> Maybe String
-showRaw bs = do
-    (encoding, string) <- B.uncons bs
-    let decoded = case encoding of 0 -> BE.decode BE.latin1 string
-                                   x | x == 1 || x == 2 -> BE.decode BE.utf16 $ removeTerminator 2 string
-                                   3 -> BE.decode BE.utf8 $ removeTerminator 1 string
-    return (T.unpack decoded)
-   
-frame :: Get Frame
+showRaw :: String -> B.ByteString -> Maybe String
+showRaw id bs =
+    if head id == 'T'
+    then do
+        (encoding, textBs) <- B.uncons bs
+        let decoded = case encoding of 0 -> BE.decode BE.latin1 textBs
+                                       x | x == 1 || x == 2 -> BE.decode BE.utf16 $ removeTerminator 2 textBs
+                                       3 -> BE.decode BE.utf8 $ removeTerminator 1 textBs
+        return $ T.unpack decoded
+    else Nothing
+
+frame :: Get (Maybe Frame)
 frame = do
-    id <- getByteString 4
-    size <- getWord32be
+    id <- unpack <$> getByteString 4
+    size <- fromIntegral <$> getWord32be
     skip 2 -- skip flags
-    bs <- getByteString $ fromIntegral size
+    bs <- getByteString size
     br <- bytesRead
-    return $ Frame (unpack id) (fromMaybe (show br) $ showRaw bs)
+    return $ fmap (\raw -> Frame id raw) $ showRaw id bs
 
 framesLeft :: Int -> Get Bool
 framesLeft size = do
     br <- bytesRead
-    -- i <- lookAhead (getByteString 4)
-    -- return $ B.any (/= 0) i && (fromIntegral br < size)
-    return (fromIntegral br < size)
+    if fromIntegral br + 10 <= size
+    then do
+        nextId <- lookAhead (getByteString 4)
+        return $ not $ B.all (== 0) nextId
+    else return False
+
 getSize :: Get Int
 getSize = do
     words <- replicateM 4 getWord8
     let size = foldl (\s x -> 128 * s + x) 0 $ map fromIntegral words
-    return size
+    return $ size + 10
 
 parseId3 :: Get [Frame]
 parseId3 = do
@@ -71,7 +78,7 @@ parseId3 = do
         skip 1 -- skip flags
         size <- getSize
         frames <- whileM (framesLeft size) frame
-        return (Frame "VER" version : Frame "SIZE" (show size): frames)
+        return $ Frame "VER" version : Frame "SIZE" (show size) : catMaybes frames
 
 insertFrame :: FrameMap -> Frame -> FrameMap
 insertFrame fm (Frame i str) = M.insert i str fm
