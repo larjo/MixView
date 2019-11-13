607c27199885f70ef096a3bc2eba4ca4bfd6613f
Lars Baker <lars.baker@uc.se>
2019-11-13 09:50:03 +0100

    Handle decoding

--- src/Id3.hs
+++ src/Id3.hs
@@ -7,16 +7,16 @@ import           Control.Monad.Loops   (whileM)
 import           Data.Binary.Get       (Get, bytesRead, getByteString,
                                         getWord32be, getWord8, lookAhead,
                                         runGet, skip)
-import qualified Data.ByteString       as B (any, ByteString, takeWhile)
+import qualified Data.ByteString       as B (any, ByteString, drop , reverse, uncons )
 import           Data.ByteString.Char8 (unpack)
 import qualified Data.ByteString.Lazy  as BL (ByteString, getContents)
-import qualified Data.ByteString.Encoding as BE (decode, latin1, utf16, utf16le)
+import qualified Data.ByteString.Encoding as BE (decode, latin1, utf16, utf8)
 import           Data.List             (intercalate)
 import qualified Data.Map              as M (Map, empty, findWithDefault,
                                              insert)
 import qualified Data.Text             as T (init, null, unpack, last)
 import           Data.Text.Encoding    (decodeUtf16LEWith, decodeUtf16BEWith)
-
+import           Data.Maybe            (fromMaybe)
 data Frame = Frame String String
 type FrameMap = M.Map String String
 
@@ -24,21 +24,30 @@ getHeader :: Get String
 getHeader = unpack <$> getByteString 3
 
 getVersion :: Get String
-getVersion = intercalate "." . map show <$> replicateM 2 getWord8
+getVersion = do
+    words <- replicateM 2 getWord8
+    return (intercalate "." ("2" : map show words))
 
-showRaw :: B.ByteString -> String
-showRaw = T.unpack . BE.decode BE.utf16
-  where
-    safeInit x = if not (T.null x) && T.last x == '\0' then T.init x else x
+removeTerminator :: Int -> B.ByteString -> B.ByteString
+removeTerminator n = B.reverse . B.drop n . B.reverse 
+--removeTerminator = B.reverse . B.dropWhile (\w8 -> w8 == 0) . B.reverse
 
+showRaw :: B.ByteString -> Maybe String
+showRaw bs = do
+    (encoding, string) <- B.uncons bs
+    let decoded = case encoding of 0 -> BE.decode BE.latin1 string
+                                   x | x == 1 || x == 2 -> BE.decode BE.utf16 $ removeTerminator 2 string
+                                   3 -> BE.decode BE.utf8 $ removeTerminator 1 string
+    return (T.unpack decoded)
+   
 frame :: Get Frame
 frame = do
     id <- getByteString 4
     size <- getWord32be
     skip 2 -- skip flags
-    skip 1
-    bs <- getByteString $ fromIntegral size - 1
-    return $ Frame (unpack id) (showRaw bs)
+    bs <- getByteString $ fromIntegral size
+    br <- bytesRead
+    return $ Frame (unpack id) (fromMaybe (show br) $ showRaw bs)
 
 framesLeft :: Int -> Get Bool
 framesLeft size = do
@@ -50,7 +59,7 @@ getSize :: Get Int
 getSize = do
     words <- replicateM 4 getWord8
     let size = foldl (\s x -> 128 * s + x) 0 $ map fromIntegral words
-    return (size + 10)
+    return size
 
 parseId3 :: Get [Frame]
 parseId3 = do
@@ -61,7 +70,8 @@ parseId3 = do
         version <- getVersion
         skip 1 -- skip flags
         size <- getSize
-        whileM (framesLeft size) frame
+        frames <- whileM (framesLeft size) frame
+        return (Frame "VER" version : Frame "SIZE" (show size): frames)
 
 insertFrame :: FrameMap -> Frame -> FrameMap
 insertFrame fm (Frame i str) = M.insert i str fm
